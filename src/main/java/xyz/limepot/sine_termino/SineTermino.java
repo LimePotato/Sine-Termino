5fcd2e67e7d1dd5ca5494e0ccd1a3775dd35bcbb
limepot <LemonPotato@protonmail.ch>
2022-08-20 14:20:58 -0600

    3 basic ore blocks created.

--- src/main/java/xyz/limepot/sine_termino/SineTermino.java
+++ src/main/java/xyz/limepot/sine_termino/SineTermino.java
@@ -5,30 +5,12 @@ import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.util.Identifier;
-
-import net.minecraft.util.registry.Registry;
-import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
+import xyz.limepot.sine_termino.block.ModBlocks;
+import xyz.limepot.sine_termino.item.ModItems;
 
 public class SineTermino implements ModInitializer {
 
 	public static final String MOD_ID = "sine-termino";
-
-	public static final Item TIN_INGOT = registerItem("tin_ingot", 
-        new Item(new QuiltItemSettings().group(ItemGroup.MISC)));
-
-    public static final Item BRONZE_INGOT = registerItem("bronze_ingot", 
-        new Item(new QuiltItemSettings().group(ItemGroup.MISC)));
-
-    public static final Item ALUMINIUM_INGOT = registerItem("aluminium_ingot", 
-        new Item(new QuiltItemSettings().group(ItemGroup.MISC)));
-
-
-		private static Item registerItem(String name, Item item){
-			return Registry.register(Registry.ITEM, new Identifier(SineTermino.MOD_ID, name), item);
-		}
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
@@ -37,6 +19,9 @@ public class SineTermino implements ModInitializer {
 
 	@Override
 	public void onInitialize(ModContainer mod) {
+
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 
 		
 		LOGGER.warn("Initialization Hijacked!!!!");
---
+++ src/main/java/xyz/limepot/sine_termino/block/ModBlocks.java
@@ -0,0 +1,35 @@
+package xyz.limepot.sine_termino.block;
+
+import org.quiltmc.qsl.block.extensions.api.QuiltBlockSettings;
+import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import xyz.limepot.sine_termino.SineTermino;
+
+public class ModBlocks {
+
+    public static final Block TIN_ORE = registerBlock("tin_ore", new Block(QuiltBlockSettings.of(Material.STONE).strength(.2f).requiresTool()), ItemGroup.MISC);
+    public static final Block BRONZE_ORE = registerBlock("bronze_ore", new Block(QuiltBlockSettings.of(Material.STONE).strength(.2f).requiresTool()), ItemGroup.MISC);
+    public static final Block ALUMINIUM_ORE = registerBlock("aluminium_ore", new Block(QuiltBlockSettings.of(Material.STONE).strength(.2f).requiresTool()), ItemGroup.MISC);
+
+    
+    private static Block registerBlock(String name, Block block, ItemGroup tab) {
+        registerBlockItem(name, block, tab);
+        return Registry.register(Registry.BLOCK, new Identifier(SineTermino.MOD_ID, name), block);
+    }
+
+    private static Item registerBlockItem(String name, Block block, ItemGroup tab) {
+        return Registry.register(Registry.ITEM, new Identifier(SineTermino.MOD_ID, name), 
+        new BlockItem(block, new QuiltItemSettings().group(tab)));
+    }
+
+    public static void registerModBlocks() {
+        SineTermino.LOGGER.debug("Registering Blocks");
+    }
+}
